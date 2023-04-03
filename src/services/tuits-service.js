a03460316df14b2bc5b7f8d23763fc7727e0df32
Akash Ganesamurthy <akashganesamurthy@Akashs-MacBook-Air.local>
2023-04-02 22:27:55 -0400

    check

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
+console.log("Check")
 console.log(API_BASE);
 
 export const createTuit = async (tuit) => {
