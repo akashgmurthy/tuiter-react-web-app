c27ad92f8d4be6b0448d9d629283193128683cca
Akash Ganesamurthy <akashganesamurthy@Akashs-MacBook-Air.local>
2023-04-09 11:33:20 -0400

    a9

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import PostSummaryItem
   from "./post-summary-item";
+import { findTuitsThunk } from "../../services/tuits-thunks";
 
 const PostSummaryList = () => {
-  const postsArray = useSelector(state => state.tuitsData)
+  const {postsArray,loading} = useSelector(state => state.tuitsData)
+  const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk())
+    }, [])
  return(
    <ul className="list-group">
      {
